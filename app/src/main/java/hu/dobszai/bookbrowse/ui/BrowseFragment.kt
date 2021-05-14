fce13c075efb41eef4d3998be9e4b8d1b3640890
Daria Dobszai <kalashnikovadoc@gmail.com>
2021-05-14 16:13:13 +0200

    navigation component and binding
    - dependencies,
    - binding layouts,
    - toolbar with titles,
    - toolbar menu,
    - navigation between fragments

---
+++ app/src/main/java/hu/dobszai/bookbrowse/ui/BrowseFragment.kt
@@ -0,0 +1,66 @@
+package hu.dobszai.bookbrowse.ui
+
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import hu.dobszai.bookbrowse.R
+import hu.dobszai.bookbrowse.databinding.FragmentBrowseBinding
+import hu.dobszai.bookbrowse.utils.appBarConfiguration
+import hu.dobszai.bookbrowse.utils.disableAppBarTitle
+
+private lateinit var binding: FragmentBrowseBinding
+
+class BrowseFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+
+        binding = FragmentBrowseBinding.inflate(inflater)
+
+        binding.open.setOnClickListener {
+            it.findNavController().navigate(R.id.detailsFragment)
+        }
+
+        setHasOptionsMenu(true)
+
+        val activity = activity as AppCompatActivity
+        activity.apply {
+            setSupportActionBar(binding.toolbar)
+            disableAppBarTitle()
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        NavigationUI.setupWithNavController(
+            binding.toolbar,
+            findNavController(),
+            appBarConfiguration
+        )
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_favorites, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return view?.let {
+            NavigationUI.onNavDestinationSelected(
+                item,
+                it.findNavController()
+            )
+        } == true || super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/hu/dobszai/bookbrowse/ui/FavoritesFragment.kt
@@ -0,0 +1,35 @@
+package hu.dobszai.bookbrowse.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import hu.dobszai.bookbrowse.R
+import hu.dobszai.bookbrowse.databinding.FragmentDetailsBinding
+import hu.dobszai.bookbrowse.databinding.FragmentFavoritesBinding
+import hu.dobszai.bookbrowse.utils.disableAppBarTitle
+
+private lateinit var binding: FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFavoritesBinding.inflate(inflater)
+        
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        NavigationUI.setupWithNavController(binding.toolbar, findNavController())
+
+    }
+}
